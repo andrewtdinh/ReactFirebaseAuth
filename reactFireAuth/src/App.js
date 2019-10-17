15cb6d562b52ddba9dabcb7e77bdfc7ebae39e90
Andrew Dinh <adinh@sightmachine.com>
2019-10-16 22:54:37 -0700

    Wrap App in AuthProvider component

--- reactFireAuth/src/App.js
+++ reactFireAuth/src/App.js
@@ -4,16 +4,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import SignUp from './SignUp';
+import { AuthProvider } from './Auth';
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={SignUp} />
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
   )
 }
 
