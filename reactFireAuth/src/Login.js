e8d51436fd369fc27fa75b2b651ca16be4fcf26b
Andrew Dinh <adinh@sightmachine.com>
2019-10-18 09:00:16 -0700

    Build up Login component

--- reactFireAuth/src/Login.js
+++ reactFireAuth/src/Login.js
@@ -3,7 +3,23 @@ import { withRouter, Redirect } from 'react-router';
 import app from './base';
 import { AuthContext } from './Auth';
 
-const Login = () => {
+const Login = ({ history }) => {
+  const handleLogin = useCallback(
+    async event => {
+      event.preventDefault();
+      const { email, password } = event.target.elements;
+      try {
+        await app
+          .auth()
+          .signInWithEmailAndPassword(email.value, password.value);
+        history.push('/');
+      } catch (error) {
+        alert(error);
+      }
+    },
+    [history]
+  )
+
   return (
     <>
       <button onClick={() => app.auth().signIn()}>Log in</button>
