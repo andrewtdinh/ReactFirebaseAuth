f616814e54092ba7f6461aeca7ef4d4b302d6922
Andrew Dinh <adinh@sightmachine.com>
2019-10-21 00:08:34 -0700

    Add uiConfig for different login options

--- firebaseAuth/src/App.js
+++ firebaseAuth/src/App.js
@@ -8,6 +8,20 @@ class App extends React.Component {
     isSignedIn: false
   }
 
+  uiConfig = {
+    signInFlow: 'popup',
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
   render() {
     return (
       <div className="App">
