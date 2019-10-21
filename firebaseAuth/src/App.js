bd7011028db380b6905f78d68724faeda03f8f7e
Andrew Dinh <adinh@sightmachine.com>
2019-10-20 23:58:36 -0700

    Remove unused import

--- firebaseAuth/src/App.js
+++ firebaseAuth/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
