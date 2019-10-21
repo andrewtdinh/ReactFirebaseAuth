91de35db9a0fe88be05725d9af330b8860030cd9
Andrew Dinh <adinh@sightmachine.com>
2019-10-20 23:38:10 -0700

    Create basic App.js with text based on user login state

--- firebaseAuth/src/App.js
+++ firebaseAuth/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isSignedIn: false
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (<Text>Signed In!</Text>) : (<Text>Not Signed In</Text>)}
+      </div>
+    )
+  }
 }
 
 export default App;
